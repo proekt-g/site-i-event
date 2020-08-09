fecbf2da2369faf1ceece6566e99d479ec1d6535
Armen <armen.armen.armen95@gmail.com>
2020-08-09 22:17:40 +0300

    09.08.20

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,18 @@
-window.addEventListener('load', function() {});
+window.addEventListener('load', function() {
+    let $contentTextInfo = document.querySelectorAll('.program .content__text-info');
+
+
+
+    if ($contentTextInfo !== null) $contentTextInfo.forEach(function(item) {
+        item.addEventListener('mouseenter', hoverContenTextInfo);
+    });
+
+    function hoverContenTextInfo() {
+        let $contentModal = document.querySelector('.content__modal');
+        $contentModal.style.height = document.querySelector('.content__box-inner').offsetHeight;
+        console.log(document.querySelector('.content__box-inner').offsetHeight + 17);
+        console.log($contentModal.style.height);
+
+        $contentModal.classList.toggle('content__modal--open');
+    }
+});
