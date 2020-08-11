5ffce132051a8f896ece5078721b7e00d88da68c
Armen <armen.armen.armen95@gmail.com>
2020-08-11 21:19:28 +0300

    11.08.20

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,10 @@
+document.onreadystatechange = function() {
+    if (document.readyState === "interactive") {
+        if (document.querySelector('.sidebar') !== null && window.innerWidth <= 1220) {
+            document.querySelector('.sidebar__content').insertAdjacentElement('afterbegin', document.querySelector('.filter'));
+        }
+    }
+}
 window.addEventListener('load', function() {
     let $contentTextInfo = document.querySelectorAll('.program .content__text-info'),
         $avatar = document.querySelectorAll('.avatar'),
@@ -8,21 +15,35 @@ window.addEventListener('load', function() {
         $dateElement = document.querySelectorAll('.date__element'),
         $tagsElementLabel = document.querySelectorAll('.tags__element-label'),
         $tagsMore = document.querySelector('.tags__more'),
-        $filterReset = document.querySelector('.filter__reset');
+        $filterReset = document.querySelector('.filter__reset'),
+        $contentBtnMore = document.querySelectorAll('.content__btn-more'),
+        $settingsIcon = document.querySelector('.settings__icon'),
+        $sidebar = document.querySelector('.sidebar'),
+        $filterClose = document.querySelector('.filter__close'),
+        $filterSubmit = document.querySelector('.filter__submit'),
+        $checkboxElementInput = document.querySelectorAll('.checkbox__element-input'),
+        $choiseElementInput = document.querySelectorAll('.choise__element-input'),
+        $buttonUp = document.querySelector('.button-up');
 
-    function autoDetectHeight(_element, coef = 0) {
+
+    function autoDetectHeight(_element, coef = 0, startHeight = 0) {
         let buffChildrenHeight = 0;
-        if (_element.style.height === '' || _element.style.height === '0px') {
+        if (_element.style.height === '' || _element.style.height === `${startHeight}px`) {
             _element.childNodes.forEach(function(item) {
                 if (item.offsetHeight !== undefined) buffChildrenHeight += (item.offsetHeight + coef);
             });
             _element.style.height = (buffChildrenHeight + 1).toString() + 'px';
-        } else _element.style.height = '0px';
+        } else _element.style.height = `${startHeight}px`;
+
 
     }
 
     function addModifierOpen(_element) {
-        _element.classList.toggle(`${_element.classList[0]}--open`);
+        if (Array.isArray(_element)) {
+            _element.forEach(function(item) {
+                item.classList.toggle(`${item.classList[0]}--open`);
+            });
+        } else _element.classList.toggle(`${_element.classList[0]}--open`);
     }
 
     function addModifierActive(_element, master = false) {
@@ -38,41 +59,122 @@ window.addEventListener('load', function() {
         else _element.textContent = textFirst;
     }
 
+    function doubleClick(_element) {
+        if (Array.isArray(_element)) {
+            _element.click();
+            _element.click();
+        } else {
+            _element.forEach(function(item) {
+                setTimeout(function() {
+                    item.click();
+                    item.click();
+                }, 200);
+            });
+        }
+    }
+
+    function blockBody() {
+        document.querySelector('body').classList.toggle('block');
+    }
+
+    function ajaxRequest(ajaxForm, url) {
+        $.ajax({
+            url: url,
+            type: "POST",
+            dataType: "html",
+            data: $("#" + ajaxForm).serialize(), // Сеарилизуем объект
+            success: function(response) { //Данные отправлены успешно
+                let result = $.parseJSON(response);
+                console.log(result);
+            },
+            error: function(response) { // Данные не отправлены
+                alert('Ошибка. Данные не отправлены.');
+            }
+        });
+    }
+
+    function checkScroll() {
+        if (window.scrollY > window.innerHeight * 1.1) $buttonUp.classList.add('button-up--active');
+        else $buttonUp.classList.remove('button-up--active');
+    }
+
+    function upPage() {
+        $('html, body').animate({ scrollTop: 0 }, 1000);
+    }
+
+
 
 
 
     if ($choise !== null) $choise.forEach(function(item) {
-        item.querySelector('.choise__title').addEventListener('click', addModifierOpen.bind(item, item, false));
-        item.querySelector('.choise__title').addEventListener('click', autoDetectHeight.bind(null, item.querySelector('.choise__inner'), 15, false));
+        item.querySelector('.choise__title').addEventListener('click', addModifierOpen.bind(item, item), false);
+        item.querySelector('.choise__title').addEventListener('click', autoDetectHeight.bind(null, item.querySelector('.choise__inner'), 15, undefined), false);
     });
     if ($checkBox !== null) $checkBox.forEach(function(item) {
-        item.querySelector('.checkbox__title').addEventListener('click', addModifierOpen.bind(item, item, false));
-        item.querySelector('.checkbox__title').addEventListener('click', autoDetectHeight.bind(null, item.querySelector('.checkbox__inner'), 15, false));
+        item.querySelector('.checkbox__title').addEventListener('click', addModifierOpen.bind(item, item), false);
+        item.querySelector('.checkbox__title').addEventListener('click', autoDetectHeight.bind(null, item.querySelector('.checkbox__inner'), 15, undefined), false);
     });
 
     if ($avatar !== null) $avatar.forEach(function(item) {
-        item.addEventListener('focus', addModifierOpen.bind(null, item.querySelector('.avatar__info'), false));
-        item.addEventListener('blur', addModifierOpen.bind(null, item.querySelector('.avatar__info'), false));
+        item.addEventListener('focus', addModifierOpen.bind(null, item.querySelector('.avatar__info')), false);
+        item.addEventListener('blur', addModifierOpen.bind(null, item.querySelector('.avatar__info')), false);
     });
     if ($contentTextInfo !== null) $contentTextInfo.forEach(function(item) {
         item.addEventListener('focus', focusContenTextInfo.bind(item, (item.closest('.content__box').querySelector('.content__box-inner').offsetHeight + 20).toString() + 'px'), false);
-        item.addEventListener('blur', focusContenTextInfo.bind(item, '0px', false));
+        item.addEventListener('blur', focusContenTextInfo.bind(item, '0px'), false);
     });
     if ($dateElement !== null) $dateElement.forEach(function(item) {
-        item.addEventListener('click', addModifierActive.bind(null, item, true, false));
+        item.addEventListener('click', addModifierActive.bind(null, item, true), false);
+        if (window.innerWidth > 1220) item.addEventListener('click', ajaxRequest.bind(null, $filterSubmit.closest('.form').className, 'test.php'), false);
     });
     if ($tagsElementLabel !== null) $tagsElementLabel.forEach(function(item) {
-        item.addEventListener('click', addModifierActive.bind(null, item, false));
+        item.addEventListener('click', addModifierActive.bind(null, item, undefined), false);
+        if (window.innerWidth > 1220) item.addEventListener('click', ajaxRequest.bind(null, $filterSubmit.closest('.form').className, 'test.php'), false);
     });
     if ($tagsMore !== null) {
-        $tagsMore.addEventListener('click', renameContentText.bind(null, $tagsMore, 'Скрыть', 'Показать все', false));
-        $tagsMore.addEventListener('click', addModifierOpen.bind(null, $tagsMore.closest('.tags').querySelector('.tags__inner'), false));
-
+        $tagsMore.addEventListener('click', renameContentText.bind(null, $tagsMore, 'Скрыть', 'Показать все'), false);
+        $tagsMore.addEventListener('click', addModifierOpen.bind(null, $tagsMore.closest('.tags').querySelector('.tags__inner')), false);
     }
     if ($filterReset !== null) $filterReset.addEventListener('click', resetFilter);
+    if ($contentBtnMore !== null) $contentBtnMore.forEach(function(item) {
+        item.addEventListener('click', autoDetectHeight.bind(null, item.closest('.content__box').querySelector('.content__modal'), undefined, 26), false);
+        item.addEventListener('click', renameContentText.bind(null, item, 'Скрыть полный текст', 'Читать полностью'), false);
+    });
+    if ($settingsIcon !== null) {
+        $settingsIcon.addEventListener('click', addModifierOpen.bind(null, [$sidebar, document.querySelector('.sidebar__content')]), false);
+        $settingsIcon.addEventListener('click', doubleClick.bind(null, document.querySelectorAll('.choise__title')), false);
+        $settingsIcon.addEventListener('click', doubleClick.bind(null, document.querySelectorAll('.checkbox__title')), false);
+        $settingsIcon.addEventListener('click', blockBody, false);
+    }
+    if ($sidebar !== null) $sidebar.addEventListener('click', checkEventTargetSidebar);
+    if ($filterClose !== null) {
+        $filterClose.addEventListener('click', addModifierOpen.bind(null, [$sidebar, document.querySelector('.sidebar__content')]), false);
+        $filterClose.addEventListener('click', blockBody, false);
+    }
+    if ($filterSubmit !== null) $filterSubmit.addEventListener('click', ajaxRequest.bind(null, $filterSubmit.closest('.form').className, 'test.php'), false);
+    if (window.innerWidth > 1220 && $checkboxElementInput !== null) {
+        $checkboxElementInput.forEach(function(item) {
+            item.addEventListener('input', ajaxRequest.bind(null, $filterSubmit.closest('.form').className, 'test.php'), false);
+        });
+    }
+    if (window.innerWidth > 1220 && $choiseElementInput !== null) {
+        $choiseElementInput.forEach(function(item) {
+            item.addEventListener('input', ajaxRequest.bind(null, $filterSubmit.closest('.form').className, 'test.php'), false);
+        });
+    }
+    window.addEventListener('scroll', checkScroll, false);
+    if ($buttonUp !== null) $buttonUp.addEventListener('click', upPage);
 
 
 
+
+
+    function checkEventTargetSidebar() {
+        if (event.target === this) {
+            addModifierOpen([this, this.querySelector('.sidebar__content')]);
+            blockBody();
+        }
+    }
 
     function resetFilter() {
         $dateElement.forEach(function(item) {
@@ -89,13 +191,16 @@ window.addEventListener('load', function() {
         $contentModal.classList.toggle('content__modal--open');
     }
 
+    function filterHeight() {
+        if ($choiseOpen !== null) $choiseOpen.forEach(function(item) {
+            autoDetectHeight(item.querySelector('.choise__inner'), 15);
+        });
+        if ($checkboxOpen !== null) $checkboxOpen.forEach(function(item) {
+            autoDetectHeight(item.querySelector('.checkbox__inner'), 15);
+        });
+    }
 
 
 
-    if ($choiseOpen !== null) $choiseOpen.forEach(function(item) {
-        autoDetectHeight(item.querySelector('.choise__inner'), 15);
-    });
-    if ($checkboxOpen !== null) $checkboxOpen.forEach(function(item) {
-        autoDetectHeight(item.querySelector('.checkbox__inner'), 15);
-    })
+    filterHeight();
 });
